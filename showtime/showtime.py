6b88be6259f92c034ff7cfd82b22c2034cb337af
remiCzn <cazinremi2@gmail.com>
2022-09-16 12:04:37 +0200

    add showtime

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -10,6 +10,16 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetListSchedules(self, request, context):
+        for schedule in self.db:
+            yield showtime_pb2.ScheduleData(date=schedule["date"], movies=schedule["movies"])
+
+    def GetMoviesByDate(self, request, context):
+        for s in self.db:
+            if request.date == s["date"]:
+                return showtime_pb2.ScheduleData(date=s["date"], movies=s["movies"])
+        return showtime_pb2.ScheduleData(date="", movies=[])
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
