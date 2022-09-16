fa6c4f45486a6feadad3ea2dc64e74a97976e739
remiCzn <cazinremi2@gmail.com>
2022-09-16 11:19:32 +0200

    movie base

--- movie/movie.py
+++ movie/movie.py
@@ -1,7 +1,6 @@
 import grpc
 from concurrent import futures
-import movie_pb2
-import movie_pb2_grpc
+from client import movie_pb2_grpc, movie_pb2
 import json
 
 class MovieServicer(movie_pb2_grpc.MovieServicer):
@@ -9,6 +8,17 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
     def __init__(self):
         with open('{}/data/movies.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["movies"]
+
+    def GetMovieByID(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
+        return movie_pb2.MovieData(title="", rating=0, director="", id="")
+
+    def GetListMovies(self, request, context):
+        for movie in self.db:
+            yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
